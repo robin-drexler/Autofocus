0d7411a14931ba19045abb85d7fee1d8d84f93d5
Robin Drexler <drexler.robin@gmail.com>
2011-08-11 19:59:41 +0200

    add missing icons, add php.net, wikipedia.org

--- cs.js
+++ cs.js
@@ -1,12 +1,17 @@
 $(function() {
+    var $firstInput = $($('input[type=text]')[0]);
     var selectors = [
-        ['amazon.', '#twotabsearchtextbox']
+        ['amazon.', '#twotabsearchtextbox'],
+        ['php.net'],
+        ['.wikipedia.org']
     ]
-
+    
     for(i in selectors) {
         if(location.href.match(selectors[i][0])) {
-            if(typeof selectors[0][1] == 'string') {
-                $(selectors[0][1]).focus();
+            if(typeof selectors[i][1] == 'string') {
+                $(selectors[i][1]).focus();
+            } else {
+                $firstInput.focus();
             }
         }
     }
