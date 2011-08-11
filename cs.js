fbaa550149c8c323af23ea2a628e3249cfa180d4
Robin Drexler <drexler.robin@gmail.com>
2011-08-11 21:36:44 +0200

    scroll back to original position after focus

--- cs.js
+++ cs.js
@@ -1,9 +1,13 @@
 $(function() {
     var matched = false;
+    var $wnd = $(window);
     var $firstInput = $($('input[type=text], input[type=search]')[0]);
     var selectors = [
         ['amazon.', '#twotabsearchtextbox'],
     ]
+    
+    var $scrollLeft = $wnd.scrollLeft();
+    var $scrollTop  = $wnd.scrollTop();
     
     for(i in selectors) {
         if(location.href.match(selectors[i][0])) {
@@ -17,5 +21,6 @@ $(function() {
     if(!matched) {
         $firstInput.focus();
     }
+    $wnd.scrollTop($scrollTop).scrollLeft($scrollLeft);
     
 });
